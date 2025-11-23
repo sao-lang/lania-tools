8a2b22cdb84ea4c4ed72121b2267f02a4828f8a5
huang <58391800+sao-lang@users.noreply.github.com>
2025-11-23 16:45:32 +0800

    Update CacheManager.ts

--- packages/tools/src/axios/CacheManager.ts
+++ packages/tools/src/axios/CacheManager.ts
@@ -1,5 +1,5 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
-
+import { generateRequestKey } from ＇./helper＇;
 interface CacheEntry {
     data: any;
     expireAt?: number;
@@ -7,16 +7,9 @@ interface CacheEntry {
 
 export class CacheManager {
     private cache: Map<string, CacheEntry> = new Map();
-    public getCacheKey(req: AxiosRequestConfig): string {
-        const method = req.method || 'get';
-        const url = req.url || '';
-        // 简单序列化，如果对参数顺序敏感请使用 qs.stringify
-        const params = req.params ? JSON.stringify(req.params) : '';
-        const data = req.data ? JSON.stringify(req.data) : ''; 
-        return `${method}:${url}:${params}:${data}`;
-    }
+    
     public get(config: AxiosRequestConfig) {
-        const key = this.getCacheKey(config);
+        const key = generateRequestKey(config);
         const entry = this.cache.get(key);
         if (entry) {
             if (!entry.expireAt || entry.expireAt > Date.now())
@@ -31,7 +24,7 @@ export class CacheManager {
         data: AxiosResponse<any>,
         ttl?: number,
     ) {
-        const key = this.getCacheKey(config);
+        const key = generateRequestKey(config);
         this.cache.set(key, {
             data,
             expireAt: ttl ? Date.now() + ttl : undefined,
@@ -42,4 +35,5 @@ export class CacheManager {
         this.cache.clear();
     }
 }
+
 
